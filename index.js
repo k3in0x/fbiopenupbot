907357f1aeeef3a2c689a338227a44ec66e8854e
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-10-22 17:22:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const fs = require("file-system");
-
 module.exports = client;
 
 const mapWhichFinnaBeAdb = new Map();
