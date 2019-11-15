0da91e4bb46d0750ca33ed9ba29a9542b0e02e59
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-15 18:37:09 +0100

    Create randomuser.js

---
+++ commands/randomuser.js
@@ -0,0 +1,58 @@
+module.exports = {
+    name: "randomuser",
+    description: "Gives general info about a random user in the server.",
+    type: "util",
+    usage: "%randomuser <settings<bot:false|me:false>>",
+
+    run: function (msg, args) {
+        const g = msg.guild;
+        const mm = g.members.filter(m => (args[1].toLowerCase().includes("bot:false") ? !m.user.bot : (args[1].toLowerCase().includes("me:false") ? m.user.id !== msg.author.id))).random
+        const m = mm.user;
+        const userroles = [];
+        const userrolesobj = [];
+        const usercolor = mm.highestRole.hexColor;
+
+        mm.roles.array().forEach(function(value) {
+            if (value.name === "@everyone") {
+                return;
+            }
+
+            userroles.push(value.name);
+            userrolesobj.push(value)
+        });
+
+        if (!userroles) {
+            usercolor = "00ff00";
+        }
+        
+        if (mm.highestRole.hexColor === "GREY" && userroles[userroles.length - 2]) {
+            usercolor = userrolesobj[userrolesobj.length - 2].hexColor;
+        }
+
+        const em = new Discord.RichEmbed() 
+        .setTitle("Random user") 
+        .setThumbnail(m.avatarURL)
+        .addField("Username:", m.tag)
+        .addField("ID:", m.id)
+        .addBlankField()
+        .addField("Nickname:", (mm.nickname !== null ? mm.nickname : m.username))
+        .addField("Roles:", userroles.join(", "))
+        .setColor(usercolor)
+        .setFooter("Requested by " + msg.author.tag);
+
+        const desc = [];
+
+        if (m.bot) {
+            desc.push("This user is a bot. You can turn off getting bots by saying `bot:false` as an argument in the command");
+        }
+        if (m.id === msg.author.id) {
+            desc.push("Hey, that's you! You can turn off getting yourself by saying `me:false` as an argument in the command");
+        }
+
+        if (desc[0]) {
+            em.setDescription(desc.join("\n"));
+        }
+
+        return msg.channel.send({embed: em});
+    }
+};
