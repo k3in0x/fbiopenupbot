1706d2dc1ef5f8c5bc00390bc7081607860ab2d7
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-18 16:24:42 +0100

    Update dev.js

--- commands/dev.js
+++ commands/dev.js
@@ -9,18 +9,23 @@ module.exports = {
         
         switch (args[1]) {
             case "eval":
-                evaluate(msg, args.slice(2).join(""));
-            break;
+                return evaluate(msg, args.slice(2).join(" "));
+            case "display":
+                return display(msg, args.slice(2).join(" "));
         }
     }
 };
 
 async function evaluate (msg, str) {
     const em = new Discord.RichEmbed().setTitle("eval").addField("Code:", str);
+
+    str = Discord.util.escapeMarkdown(str);
     
     try {
         let evaled = eval(str);
-        evaled = typeof evaled.then === "function" ? await evaled : evaled;
+        while (typeof evaled.then === "function") {
+            evaled = await evaled
+        }
         const evaledText = typeof evaled === "string" ? evaled : require("util").inspect(evaled, {depth: 1});
         em.addField("Success", `\`\`\`js\n${evaledText}\n\`\`\``);
         em.setColor("00ff00");
@@ -30,4 +35,8 @@ async function evaluate (msg, str) {
     }
     
     return msg.channel.send({embed: em});
+}
+
+function display (msg, str) {
+
 }
