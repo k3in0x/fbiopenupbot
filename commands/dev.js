21749631f6ca1a5b7660d4a9abbca98888c963f5
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-18 16:48:16 +0100

    Update dev.js

--- commands/dev.js
+++ commands/dev.js
@@ -19,14 +19,18 @@ module.exports = {
 async function evaluate (msg, str) {
     const em = new Discord.RichEmbed().setTitle("eval").addField("Code:", str);
 
-    str = Discord.util.escapeMarkdown(str);
+    str = Discord.escapeMarkdown(str);
     
     try {
+        let evaledText = "";
+        console.log = l => {
+            evaledText += l + "\n";
+        };
         let evaled = eval(str);
         while (typeof evaled.then === "function") {
             evaled = await evaled
         }
-        const evaledText = typeof evaled === "string" ? evaled : require("util").inspect(evaled, {depth: 1});
+        evaledText += typeof evaled === "string" ? evaled : require("util").inspect(evaled, {depth: 1});
         em.addField("Success", `\`\`\`js\n${evaledText}\n\`\`\``);
         em.setColor("00ff00");
     } catch (e) {
@@ -37,6 +41,21 @@ async function evaluate (msg, str) {
     return msg.channel.send({embed: em});
 }
 
-function display (msg, str) {
+asnyc function display (msg, str) {
+    str = Discord.escapeMarkdown(str);
 
+    try {
+        let evaled = eval(str);
+        while (typeof evaled.then === "function") {
+            evaled = await evaled;
+        }
+        let output = [...Discord.splitMessage(recursiveTextify(evaled)]);
+        output.map(message.channel.send);
+    } catch (e) {
+        return msg.channel.send(e.stack);
+    } 
+}
+
+function recursiveTextify (e) {
+    return Object.keys(e).map((key, index) => [key, (["string", "boolean", undefined, "number"].includes(typeof Object.values(e)[index]) ? Object.values(e)[index] : recursiveTextify(Object.values(e)[index]))].join(":\n")).join("\n\n");
 }
