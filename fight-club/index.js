8dc0159ef56150a51fc5b6d5df5fdc1e7c610607
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-20 17:06:45 +0100

    Update index.js

--- fight-club/index.js
+++ fight-club/index.js
@@ -34,6 +34,29 @@ async function evaluate (msg, str) {
     }
 }
 
-function textify (obj) {
-    return;
+function textify (e) {
+    return Object.keys(e).map((key, index) => [key, (["string", "boolean", undefined, "number"].includes(typeof Object.values(e)[index]) ? Object.values(e)[index] : recursiveTextify(Object.values(e)[index]))]);
+}
+
+function recursiveTextify (obj) {
+    if (typeof obj === "array") return obj.join("\n");
+
+    if (obj instanceof Map || obj instanceof Discord.Collection) {
+        const values = [];
+
+        for (const [key, val] of obj.entries()) {
+            values.push([key, /*(typeof val === "object" ? textify(val) : */val/*)*/]);
+        }
+
+        obj = values;
+    }
+
+    if (obj instanceof Object) {
+        const values = Object.values(obj);
+        const keys = Object.keys(obj);
+
+        obj = keys.map((key, index) => [key, /*(typeof values[index] === "object" ? textify(values[index]) : */values[index]/*)*/]);
+    }
+
+    return obj;
 }
